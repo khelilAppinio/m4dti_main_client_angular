92688cc7077a07f84f951e483dc5a8d678c4c0ec
Khelil Mouhssin Amari <khelil.amari@appinio.com>
2019-12-09 09:32:45 +0100

    remove unused modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,27 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { LoginFormComponent } from './components/login-form/login-form.component';
 
-import { MatCardModule, MatInputModule, MatButtonModule, MatCheckboxModule, MatSlideToggleModule} from '@angular/material';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 @NgModule({
 	declarations: [
 		AppComponent,
-		LoginFormComponent
 	],
 	imports: [
 		BrowserModule,
 		AppRoutingModule,
 		BrowserAnimationsModule,
-
-		MatCardModule,
-		MatInputModule,
-		MatButtonModule,
-		MatCheckboxModule,
-		MatSlideToggleModule,
-		FormsModule,
-    ReactiveFormsModule,
 	],
 	providers: [],
 	bootstrap: [AppComponent]
