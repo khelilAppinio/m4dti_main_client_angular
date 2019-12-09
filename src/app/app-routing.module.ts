804f85327bd95f7b5df9cb620c732872e0372566
Khelil Mouhssin Amari <khelil.amari@appinio.com>
2019-12-09 09:32:25 +0100

    add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+	{ path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) },
+	{ path: 'login', loadChildren: () => import('./pages/login/login.module').then(m => m.LoginModule) }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+	imports: [RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })],
+	exports: [RouterModule]
 })
 export class AppRoutingModule { }
