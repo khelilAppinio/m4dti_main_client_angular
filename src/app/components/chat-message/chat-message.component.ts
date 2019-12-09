d2b39932094995958bc55d3caeb7e08f6428b562
Khelil Mouhssin Amari <khelil.amari@appinio.com>
2019-12-09 09:33:37 +0100

    add new component

---
+++ src/app/components/chat-message/chat-message.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+	selector: 'm4dti-chat-message',
+	templateUrl: './chat-message.component.html',
+	styleUrls: ['./chat-message.component.scss']
+})
+export class ChatMessageComponent implements OnInit {
+
+	constructor() { }
+
+	ngOnInit() {
+	}
+
+}
