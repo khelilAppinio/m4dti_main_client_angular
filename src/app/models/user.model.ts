d775c1c73db0bc2a0ed9c1e0a6369a18a8245bcf
Khelil Mouhssin Amari <khelil.amari@appinio.com>
2019-12-17 18:20:17 +0100

    switch User interface to class

--- src/app/models/user.model.ts
+++ src/app/models/user.model.ts
@@ -1,5 +1,16 @@
-export interface User {
+export class User {
 	username: string;
 	avatar: string;
 	status: 'offline' | 'online';
+
+	constructor(username: string, avatar?: string, status?: UserStatus) {
+		this.username = username;
+		this.avatar = this.avatar ? this.avatar : 'https://picsum.photos/200/300?random=' + Math.floor(Math.random() * Math.floor(100));
+		this.status = this.status ? this.status : UserStatus.ONLINE;
+	}
+}
+
+enum UserStatus {
+	OFFLINE = 'offline',
+	ONLINE = 'online'
 }
