f0b973912cfd718abcd65ef07531b990082cc7ee
Khelil Mouhssin Amari <khelil.amari@appinio.com>
2019-12-19 15:44:09 +0100

    add userView class with some methods

--- src/app/models/user.model.ts
+++ src/app/models/user.model.ts
@@ -1,13 +1,57 @@
+import { Message } from './message.model';
+
 export class User {
-	username: string;
-	avatar: string;
-	status: 'offline' | 'online';
-	focused: boolean;
-	constructor(username: string,focused?: boolean, avatar?: string, status?: UserStatus) {
+	private id: string;
+	private username: string;
+	private avatar: string;
+	private status: 'offline' | 'online';
+	private sourceSocketId: string;
+
+	constructor(username: string, sourceSocketId: string, focused?: boolean, avatar?: string, status?: UserStatus) {
 		this.username = username;
+		this.sourceSocketId = sourceSocketId;
 		this.avatar = this.avatar ? this.avatar : 'https://picsum.photos/200/300?random=' + Math.floor(Math.random() * Math.floor(100));
 		this.status = this.status ? this.status : UserStatus.ONLINE;
+	}
+
+	public getSourceSocketId(): string {
+		return this.sourceSocketId;
+	}
+
+	public getId(): string {
+		return this.id;
+	}
+
+	public getUsername(): string {
+		return this.username;
+	}
+}
+
+export class UserView extends User {
+
+	private focused: boolean;
+	private messages: Message[];
+
+	constructor(username: string, sourceSocketId: string, messages: Message[], focused?: boolean, avatar?: string, status?: UserStatus) {
+		super(username, sourceSocketId, focused, avatar, status);
 		this.focused = this.focused ? this.focused : false;
+		this.messages = messages;
+	}
+
+	public pushMessage(message: Message) {
+		this.messages.push(message);
+	}
+
+	public getMessages(): Message[] {
+		return this.messages;
+	}
+
+	public setFocused(focused: boolean): void {
+		this.focused = focused;
+	}
+
+	public isFocused(): boolean {
+		return this.focused;
 	}
 }
 
