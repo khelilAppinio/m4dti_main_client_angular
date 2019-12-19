ec7bd9d12b614ca68169087643fb0bc5feeb25ed
Khelil Mouhssin Amari <khelil.amari@appinio.com>
2019-12-19 15:43:32 +0100

    switch message interface to class

--- src/app/models/message.model.ts
+++ src/app/models/message.model.ts
@@ -1,5 +1,13 @@
-export interface Message {
+export class Message {
 	admin: boolean;
 	body: string;
 	date: string; // TODO: change to switable type.
+	sourceSocketId?: string;
+
+	constructor(body: string, date: string, admin: boolean, sourceSocketId?: string) {
+		this.admin = admin;
+		this.body = body;
+		this.date = date;
+		this.sourceSocketId = sourceSocketId;
+	}
 }
