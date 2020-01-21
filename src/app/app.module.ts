e32304f1c951080caa6aafe20d69b06a1022f6a5
Khelil Mouhssin Amari <khelil.amari@appinio.com>
2020-01-21 10:37:33 +0100

    add HttpClientModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
+import { HttpClientModule } from '@angular/common/http';
 const config: SocketIoConfig = { url: 'http://localhost:4444', options: {} };
 
 @NgModule({
@@ -16,7 +17,8 @@ const config: SocketIoConfig = { url: 'http://localhost:4444', options: {} };
 		BrowserModule,
 		AppRoutingModule,
 		BrowserAnimationsModule,
-		SocketIoModule.forRoot(config)
+		SocketIoModule.forRoot(config),
+		HttpClientModule
 	],
 	providers: [],
 	bootstrap: [AppComponent]
