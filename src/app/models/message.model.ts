ec6bdfb25d0a9ae162c84eca58ce00fdf536ec55
Khelil Mouhssin Amari <khelil.amari@appinio.com>
2020-01-23 21:09:03 +0100

    add media

--- src/app/models/message.model.ts
+++ src/app/models/message.model.ts
@@ -1,13 +1,15 @@
 export class Message {
-	admin: boolean;
+	isAdmin: boolean;
 	body: string;
 	date: string; // TODO: change to switable type.
 	sourceSocketId?: string;
+	mediaUrl?: string;
 
-	constructor(body: string, date: string, admin: boolean, sourceSocketId?: string) {
-		this.admin = admin;
+	constructor(body: string, date: string, isAdmin: boolean, sourceSocketId?: string, mediaUrl?: string) {
+		this.isAdmin = isAdmin;
 		this.body = body;
 		this.date = date;
 		this.sourceSocketId = sourceSocketId;
+		this.mediaUrl = mediaUrl;
 	}
 }
